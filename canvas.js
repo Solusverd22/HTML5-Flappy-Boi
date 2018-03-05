cf76113cbd23ddef22d52df8e7b72e8d88f1ed8c
Solusverd22 <32770672+Solusverd22@users.noreply.github.com>
2018-03-05 21:22:36 +0000

    Target Aim Added
    changed from flappy boi to target aim game

--- canvas.js
+++ canvas.js
@@ -21,6 +21,27 @@ var c = canvas.getContext('2d');
 //    c.strokeStyle = 'lightblue';
 //    c.stroke();
 //}
+var input = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener('mousemove',
+    function(event){
+    input.x = event.x;
+    input.y = event.y;
+    })
+
+window.addEventListener('click',
+    function () {
+        if (checkClicked(circle.x, circle.y, circle.radius)) {
+            console.log('good job');
+            timerStart = d.getTime();
+            flash = true;
+        }
+    })
+
+
 
 function Circle(x, y, radius, colour) {
     this.x = x;
@@ -29,23 +50,63 @@ function Circle(x, y, radius, colour) {
     this.colour = colour;
 
     this.draw = function () {
+        //outer circle
         c.beginPath();
         c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
         c.strokeStyle = colour;
+        c.fillStyle = colour;
+        c.fill();
+        //inner circle
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius/2, Math.PI * 2, false);
+        c.strokeStyle = 'red';
+        c.fillStyle = 'red';
+        c.fill();
         c.stroke();
     }
 
-    this.update = function (t) {
+    this.update = function () {
         //the cos and sin functions create circular movement
-        this.x = 400 + (300 * Math.cos(t));
-        this.y = 400 + (300 * Math.sin(t));
+        this.x = 400 + (300 * Math.cos(d.getTime()/1000));
+        this.y = 400 + (300 * Math.sin(d.getTime()/1000));
 
         this.draw();
     }
 }
 
+function checkClicked(x,y,r) {
+    var xDisp = input.x - x;
+    var yDisp = input.y - y;
+    var c = (xDisp * xDisp) + (yDisp * yDisp);
+    if (Math.abs(c) < (r * r)) {
+        return true;
+    } else {
+        return false;
+    }  
+}
+
+function animateFlash() {
+    h = window.innerHeight;
+    w = window.innerWidth;
+    if (flash) {
+        if ((d.getTime() - timerStart) < 100) {
+            c.fillStyle = 'red';
+            c.fillRect(0, 0, w, h);
+        }else if (d.getTime() - timerStart < 200) {
+            c.fillStyle = 'white';
+            c.fillRect(0, 0, w, h);
+        }else if (d.getTime() - timerStart < 300) {
+
+            flash = false;
+        }
+    }
+}
+
 var circle = new Circle(300, 300, 40, 'white');
-var counter = 0;
+var flash = false;
+var timerStart = 0;
+var d = new Date();
 
 function animate() {
     requestAnimationFrame(animate);
@@ -54,10 +115,12 @@ function animate() {
     canvas.height = window.innerHeight;
     //clears canvas every frame
     c.clearRect(0, 0, innerWidth, innerHeight);
+    //update time
+    d = new Date();
 
-    counter += 0.05;
+    animateFlash();
 
-    circle.update(counter);
+    circle.update();
 }
 
 animate();
